0528da96134fce74b19227751dab49725f947321
John Azinheira <jva357@gmail.com>
2015-03-08 19:23:28 -0400

    Move all labs to single repo

---
+++ Lab5_FunctionsInC/main.d
@@ -0,0 +1,4 @@
+.\main.o: main.c
+.\main.o: C:\Program Files (x86)\Keil\ARM\ARMCC\bin\..\include\stdio.h
+.\main.o: uart.h
+.\main.o: TExaS.h
---
+++ Lab5_FunctionsInC/uart.d
@@ -0,0 +1,3 @@
+.\uart.o: UART.c
+.\uart.o: UART.h
+.\uart.o: C:\Program Files (x86)\Keil\ARM\ARMCC\bin\..\include\stdio.h
